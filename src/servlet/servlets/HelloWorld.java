61dceacbaed2392817325d09126c15efe07b37d9
OHSEJIN <jinio88@gmail.com>
2018-03-29 20:28:53 +0900

    서블릿 작성

---
+++ src/servlet/servlets/HelloWorld.java
@@ -0,0 +1,43 @@
+package servlet.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorld implements Servlet {
+  ServletConfig config;
+
+  @Override
+  public void destroy() {
+    System.out.println("destroy()");
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    System.out.println("getServletConfig()");
+    return this.config;
+  }
+
+  @Override
+  public String getServletInfo() {
+    System.out.println("getServletInfo()");
+    return "version=1.0";
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    System.out.println("init() 호출!");
+    this.config = config;
+    
+  }
+
+  @Override
+  public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    System.out.println("service()");
+  }
+
+}
