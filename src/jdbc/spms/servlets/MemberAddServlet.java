8e21f0eca8cc1b8006da52b2776a5c84c727dc0f
OHSEJIN <jinio88@gmail.com>
2018-03-31 23:55:07 +0900

    DD파일 저장, updateServlet

--- src/jdbc/spms/servlets/MemberAddServlet.java
+++ src/jdbc/spms/servlets/MemberAddServlet.java
@@ -35,6 +35,7 @@ public class MemberAddServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+    request.setCharacterEncoding("UTF-8");
     Connection conn = null;
     PreparedStatement stmt = null;
     
@@ -52,12 +53,19 @@ public class MemberAddServlet extends HttpServlet {
       stmt.setString(3, request.getParameter("name"));
       stmt.executeUpdate();
       
+      // 리다이렉트 방법
+      // response.sendRedirect("list");
+      
+      // 리프레시 방법
       response.setContentType("text/html; charset=UTF-8");
       PrintWriter out = response.getWriter();
       out.println("<html><head><title>회원등록결과</title></head>");
       out.println("<body>");
       out.println("<p>등록 성공입니다!</p>");
       out.println("</body></html>");
+      
+      // 리프래시 응답해더
+      response.addHeader("Refresh", "1;url=list"); // 1초뒤 /list 로
       
     } catch (Exception e) {
       throw new ServletException(e);
