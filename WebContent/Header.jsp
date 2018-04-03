c7bed584588d892eae0d779981a6dd98381cceff
OHSEJIN <jinio88@gmail.com>
2018-04-03 20:30:08 +0900

    HttpSession 로그아웃

--- WebContent/Header.jsp
+++ WebContent/Header.jsp
@@ -6,5 +6,8 @@ Member member = (Member)session.getAttribute("member");
 %>
 <div style="background-color:#00008b;color:#ffffff;height:20px;padding: 5px;">
 SPMS(Simple Project Management System)
+<%=member.getName()%>
+<a style="color:white;" 
+  href="<%=request.getContextPath()%>/auth/logout">로그아웃</a>
 </span>
 </div>
---
+++ src/servlet/spms/servlets/LogOutServlet.java
@@ -0,0 +1,30 @@
+package servlet.spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/auth/logout")
+public class LogOutServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    session.invalidate();
+    
+    response.sendRedirect("login");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    super.doPost(req, resp);
+  }
+  
+}
